5a45460bf27c1be73fac7c723cf3ba54a0972a73
luisr1026 <luisr1026@gmail.com>
2020-01-29 15:56:08 -0800

    update student

--- models/student model.js
+++ models/student model.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -8,46 +7,12 @@ const userSchema = new Schema({
 		type: string,
 		required: true
 	},
-	institution
+	institution:{
+		type:  string,
+		required: true
+	}
 		
 }, {
   timestamps: true,
 });
 
-//authenticate input against database
-userSchema.statics.authenticate = function (email, password, callback) {
-  User.findOne({ email: email })
-    .exec(function (err, user) {
-      if (err) {
-        return callback(err)
-      } else if (!user) {
-        var err = new Error('User not found.');
-        err.status = 401;
-        return callback(err);
-      }
-      bcrypt.compare(password, user.password, function (err, result) {
-        if (result === true) {
-          return callback(null, user);
-        } else {
-          return callback();
-        }
-      })
-    });
-}
-
-userSchema.pre('save',function(next){
-  var user = this;
-  bcrypt.hash(user.password, 10, function(err, hash){
-    if(err){
-      return next(err);
-    }
-    user.password = hash;
-    next();
-  })
-});
-
-const User = mongoose.model('User', userSchema);
-
-
-
-module.exports = User;
