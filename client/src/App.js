42699d705d755f8909cd4497f54a68a07ad147d9
Diego Gonzalez <dgonzalez21@users.noreply.github.com>
2020-02-02 13:01:20 -0800

    Create Account/Login/Logout
    everything works except logout. Can't seem to get it to work. Also installed bootstrap 4.0 css only

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React, { Fragment } from "react";
 import Navbar from "./components/layout/Navbar";
+import Login from "./components/layout/Login";
 import Landing from "./components/layout/Landing";
+import CreateAccount from "./components/layout/CreateAccount";
+import LoggedOut from "./components/layout/LoggedOut";
 import "./App.css";
 
 const App = () => (
   <Fragment>
+    <LoggedOut/>
     <Navbar />
-    <Landing />
+    
+    <Landing/>
+    <LoggedOut/>
   </Fragment>
 );
 
