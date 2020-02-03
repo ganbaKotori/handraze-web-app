fd9d166d3053690a58f0e73659e711d857c98219
Alex <43049608+ganbaKotori@users.noreply.github.com>
2020-02-03 14:09:03 -0800

    adjusted user route and server.js code

--- routes/User.js
+++ routes/User.js
@@ -1,60 +1,88 @@
-const router = require('express').Router();
-let User = require('../models/user.model');
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.model");
 
-router.route('/').get((req, res) => {
+router.route("/").get((req, res) => {
   User.find()
     .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
-router.route('/login').post((req, res, next) => {
-  User.authenticate(req.body.logemail. req.body.logpassword, function(error, user) {
-    if(error || !user) {
-      var err = new Error('Wrong email or password.');
+router.route("/login").post((req, res, next) => {
+  User.authenticate(req.body.logemail, req.body.logpassword, function(
+    error,
+    user
+  ) {
+    if (error || !user) {
+      var err = new Error("Wrong email or password.");
       err.status = 401;
       return next(err);
     } else {
       req.session.userID = user._id;
-      res.status(200).json('Login successful');
+      res.status(200).json("Login successful");
     }
   });
 });
 
-router.route('/addUser').post((req, res) => {
-  const firstName  = req.body.firstName;
-  const lastName   = req.body.lastName;
-  const email      = req.body.email;
-  const password   = req.body.password;
+// @route   POST API/User/Register
+// @desc    Register user
+// @access  Public
+router.route("/register").post((req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const username = req.body.username;
+  const u_password = req.body.password;
 
-  const newUser = new User({firstName, lastName, email, password});
+  const newUser = new User({
+    email,
+    username,
+    u_password,
+    lastName,
+    firstName
+  });
 
-  newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+  newUser
+    .save()
+    .then(() => res.json("User added!"))
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
-router.route('/:id;').get((req, res) => {
+// @route   GET API/User/:id
+// @desc    find user
+// @access  Public
+router.get("/:id", (req, res) => {
   User.findById(req.params.id)
     .then(user => res.json(user))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
-router.route('/:id').get((req, res) => {
-  User.findByIdAndDelete(req.params.id)
-    .then(() => res.json('User deleted!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+// @route   DELETE API/User/Delete
+// @desc    find user
+// @access  Public
+router.delete("/:id", async (req, res) => {
+  try {
+    await User.findOneAndRemove({ _id: req.params.id });
+    res.json({ msg: "User deleted!" });
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
 });
 
-router.route('/update/:id').post((req, res) => {
-  User.findById(re.params.id)
+// @route   POST API/User/Update
+// @desc    find user
+// @access  Public
+router.post("/update/:id", (req, res) => {
+  User.findById(res.params.id)
     .then(user => {
       user.email = req.body.email;
 
-      user.save()
-        .then(() => res.json('User updated!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+      user
+        .save()
+        .then(() => res.json("User updated!"))
+        .catch(err => res.status(400).json("Error: " + err));
     })
-    .catch(err => res.status(400).json('Error: ' + err));
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+const usersRouter = require("./routes/User");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", usersRouter);
 
 const port = 3000; // go to http://localhost:3000
+
+const uri = "mongodb://alex:alex123@ds117145.mlab.com:17145/handraze-dev";
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.get("/", (req, res) => {
   //req = requirement ; res = response
