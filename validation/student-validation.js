83eb99a7c118f945c5f99559aa96aa3576b4e357
MrDrDAVID <garzadavid26@yahoo.com>
2020-02-05 15:54:03 -0800

    Created user and student validation

---
+++ validation/student-validation.js
@@ -0,0 +1,36 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateRegisterInput(data) {
+    let errors = {};
+
+    data.id = !isEmpty(data.id) ? data.id : "";
+    data.year = !isEmpty(data.year) ? data.year : "";
+    data.institution = !isEmpty(data.institution) ? data.institution : "";
+    var years = ["Freshman", "Sophmore", "Junior", "Senior"];
+
+    if (Validator.isEmpty(data.id)) {
+        errors.id = "ID is required";
+    }
+
+    if (Validator.isEmpty(data.year)) {
+        if (!years.includes(data.year)) {
+            errors.year = "Incorrect year";
+        }
+
+        errors.year = "Year is required";
+    }
+
+    if (Validator.isEmpty(data.institution)) {
+        errors.institution = "Institution is required";
+    }
+
+    if (!years.includes(data.year)) {
+        errors.year = "Incorrect year";
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    };
+};
