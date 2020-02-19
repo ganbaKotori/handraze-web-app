c9d557c2720a0ce17ca10f7e73d9f9862d483d97
studentuser09 <fido.skylar@gmail.com>
2020-02-19 13:19:22 -0800

    Deleted sendgrid.env, removed require('config') from auth, and updated AWS keys

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const bcrypt = require("bcrypt-nodejs");
 const jwt = require("jsonwebtoken");
-const config = require("config");
 
 const User = require("../models/user.model");
 
