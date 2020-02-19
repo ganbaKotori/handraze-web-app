ea768695ae2ec236ee4a208153691865630e783c
Alex <43049608+ganbaKotori@users.noreply.github.com>
2020-02-18 22:12:37 -0800

    added logo to navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,22 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import handraze from "../../img/handrazelogow.png";
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-orange">
-      <h1>
-        <Link to="/">
-          <i className="fas fa-code"></i> Handraze
-        </Link>
-      </h1>
-      <ul>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
+      <div className="flexcontainer">
+        <div>
+          <Link to="/">
+            <i className="fas fa-code"></i>{" "}
+            <img src={handraze} className="logo" alt="student" />
+          </Link>
+        </div>
+
+        <div className>
+          <ul className="navbarText">
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
