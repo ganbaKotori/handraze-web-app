97d09a65954317af9019f1282fdd72c6f33298bd
Alex <43049608+ganbaKotori@users.noreply.github.com>
2020-02-23 18:13:57 -0800

    added PDF generator

--- client/src/components/layout/Lecture.js
+++ client/src/components/layout/Lecture.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 import { PDF } from "./PDFviewer";
+import htmlpdf from "html2pdf.js";
 
 class Lecture extends Component {
   constructor(props) {
     super(props);
     this.state = { showPopup: false };
+    this.test = this.test.bind(this);
+  }
+
+  test() {
+    // Get the element.
+    var element = document.getElementById("test");
+
+    // Generate the PDF.
+    htmlpdf()
+      .from(element)
+      .set({
+        margin: 1,
+        filename: "test.pdf",
+        html2canvas: { scale: 2 },
+        jsPDF: {
+          orientation: "portrait",
+          unit: "in",
+          format: "letter",
+          compressPDF: true
+        }
+      })
+      .save();
   }
 
   togglePopup() {
@@ -15,6 +38,8 @@ class Lecture extends Component {
   render() {
     return (
       <div className="pdf">
+        <button onClick={this.test}>Generate PDF</button>
+        <div id="test">This is a test</div>
         <br />
         <PDF />
       </div>
