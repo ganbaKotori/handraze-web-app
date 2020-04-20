1746b097d804b8c44be971ae93890b02fc67714c
Diego Gonzalez <dgonzalez21@users.noreply.github.com>
2020-04-19 20:16:55 -0700

    Fix for dayOfWeek
    fix for day of week

--- client/src/components/views/Student/CourseCard.js
+++ client/src/components/views/Student/CourseCard.js
@@ -4,31 +4,44 @@ import { Link } from "react-router-dom";
 
 export class CourseCard extends Component {
   constructor(props) {
+    if(props.inputValue.dayOfWeek != null){
+      if(String(props.inputValue.dayOfWeek).charAt(0) == "T"){
+        props.inputValue.dayOfWeek = String(props.inputValue.dayOfWeek).substring(0,2);
+
+      }else{
+        props.inputValue.dayOfWeek = String(props.inputValue.dayOfWeek).charAt(0);
+      }
+      
+    };
+    
     super(props);
     this.state = {
       inputValue: []
     };
+    
   }
+
+
   render() {
     return (
       <div>
-<Link className="link" to={`/course/${this.props.inputValue._id}`}>
-<div class="card">
-              <div class="card-header bg-primary">
-                <h5>{this.props.inputValue.title}</h5>
-                <h5>{this.props.inputValue.description}</h5>
-                <h5>{this.props.inputValue.dayOfWeek} {this.props.inputValue.classStart}-{this.props.inputValue.classEnd}</h5>
-              </div>
-              <div class="card-body">
-                <p class="card-text text-muted">
-                  No New Questions | No New Answer | No New Notes
-                </p>
-              </div>
-              <div class="card-footer text-muted"></div>
-            </div>
-            </Link>
-            <br/>
-      </div>
+            <Link className="link" to={`/course/${this.props.inputValue._id}`}>
+                  <div class="card">
+                          <div class="card-header bg-primary">
+                            <h5>{this.props.inputValue.title}</h5>
+                            <h5>{this.props.inputValue.description}</h5>
+                            <h5>{this.props.inputValue.dayOfWeek} {this.props.inputValue.classStart}-{this.props.inputValue.classEnd}</h5>
+                          </div>
+                          <div class="card-body">
+                            <p class="card-text text-muted">
+                              No New Questions | No New Answer | No New Notes
+                            </p>
+                          </div>
+                          <div class="card-footer text-muted"></div>
+                  </div>
+              </Link>
+                <br/>
+        </div>
       
     );
   }
