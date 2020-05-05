0ba609877fc09adb2838e5047c098c1c459ca782
Diego Gonzalez <dgonzalez21@users.noreply.github.com>
2020-05-04 21:15:19 -0700

    chat box fix

--- client/src/components/views/Lecture/ChatPage.js
+++ client/src/components/views/Lecture/ChatPage.js
@@ -13,29 +13,60 @@ export class ChatPage extends Component {
           inputValue: "",
           chatMessage: "",
           chatRoom: ""
-      }
+         }
+         
+
       }
 
+    scrollToBottom = () => {
+        if(this.messageList != undefined){
+
+        
+        const scrollHeight = this.messageList.scrollHeight;
+
+        const height = this.messageList.clientHeight;
+
+        const maxScrollTop = scrollHeight - height;
+        
+        this.messageList.scrollTop = maxScrollTop;
+        }
+    
+    }
     componentDidMount() {
+        this.scrollToBottom();
         let server = "http://localhost:3000";
         this.props.dispatch(getChats(this.props.inputValue));
         this.socket = io(server);
+
         this.socket.on("Output Chat Message", messageFromBackEnd => {
             console.log(messageFromBackEnd)
             this.props.dispatch(afterPostMessage(messageFromBackEnd));
+            this.scrollToBottom();
         })
+
+
+
     }
 
     hanleSearchChange =(e) => {
         this.setState({
             chatMessage: e.target.value
         })
+        this.scrollToBottom();
+
+
+
     }
     renderCards = () =>
+        
         this.props.chats.chats
-        && this.props.chats.chats.map((chat) => (
+        && this.props.chats.chats.map((chat) => 
+        (
+
             <ChatCard key={chat._id}  {...chat} />
-        ));
+            
+        )
+    );
         
     submitChatMessage = (e) => {
         e.preventDefault();
@@ -58,26 +89,29 @@ export class ChatPage extends Component {
             type
         });
         this.setState({ chatMessage: "" })
+        
+
+
     }
 
     render() {
         return (
             <React.Fragment>
-                {console.log(this.props.inputValue)}
+                {console.log(this.props.inputValue) }
                 <div >
             <div class="newsfeed">
-            <div class="list-group notes-board"> {this.renderCards()}
-                    <div
-                        ref={el => {
-                            this.messagesEnd = el;
-                        }}
-                        style={{ float: "left", clear: "both" }}
-                    />
-                    </div>
+            <div class="list-group notes-board" ref={(div) => {
+          this.messageList = div;
+        }}> 
+                    {this.renderCards()}
+                    
+            </div>
                    
                   <div className="list-group-item list-group-item-action flex-column align-items-start">
                   <Form layout="inline" onSubmit={this.submitChatMessage}>
-                  <div class="input-group mb-3">
+                  <div class="input-group ">
+
+                
                   <input
       
                     class="form-control form-control-lg"
@@ -100,8 +134,21 @@ export class ChatPage extends Component {
                     >
                       Send {this.state.inputValue}
                     </button>
+                    
                   </div>
                 </div>
+
+                <div class="input-group input-group-sm mb-3"  style={{width:"90%",margin:"auto", paddingTop:"5px"}}>
+                    <div class="input-group-prepend">
+                        <label class="input-group-text" for="inputGroupSelect01">Send As</label>
+                    </div>
+                    <select class="custom-select" id="inputGroupSelect01">
+                        <option selected>Choose...</option>
+                        <option value="1">Self</option>
+                        <option value="2">Anonymous</option>
+                        
+                    </select>
+                    </div>
                     </Form>
                   </div>
             </div>
@@ -112,7 +159,9 @@ export class ChatPage extends Component {
 }
 
 const mapStateToProps = state => {
+    
     console.log(state)
+    
     return {
         user1: state.auth,
         chats: state.chat
