d15d68fea7e8a142383d9aa72176f04c5c5c3057
Alex <43049608+ganbaKotori@users.noreply.github.com>
2020-05-14 16:03:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -71,10 +71,10 @@ connection.once("open", () => {
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build'))
+})
 }
 
 io.on("connection", socket => {
