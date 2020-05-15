26f102daabf9e0a1438f56bae1a53c95861769b1
Alex <43049608+ganbaKotori@users.noreply.github.com>
2020-05-14 18:08:10 -0700

    hope this works

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { Chat } = require("./models/chat.model");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 const path = require("path");
+const server = require("http").createServer(app);
 
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
-
-
 
 app.use(cors());
 
@@ -17,13 +16,13 @@ const authRouter = require("./routes/Auth");
 const chatRouter = require("./routes/Chat");
 const courseRouter = require("./routes/Course");
 const discussionQuestionRouter = require("./routes/DiscussionQuestion");
-const fileRouter = require("./routes/FileUpload");
+//const fileRouter = require("./routes/FileUpload");
 const instructorRouter = require("./routes/Instructor");
 const lectureRouter = require("./routes/Lecture");
 const studentRouter = require("./routes/Student");
 const userRouter = require("./routes/User");
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:5000"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
@@ -35,7 +34,7 @@ app.use(function(req, res, next) {
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cookieParser());    
+app.use(cookieParser());
 
 // SETUP ROUTES HERE
 app.use("/api/auth", authRouter);
@@ -45,12 +44,12 @@ app.use("/api/instructors", instructorRouter);
 app.use("/api/lectures", lectureRouter);
 app.use("/api/students", studentRouter);
 app.use("/api/questions", discussionQuestionRouter);
-app.use("/api/upload", fileRouter);
+//app.use("/api/upload", fileRouter);
 app.use("/api/users", userRouter);
 
 app.use(express.json());
 
-let rooms = {}
+let rooms = {};
 
 // CONNECT TO MONGODB
 const uri = "mongodb://alex:alex123@ds235378.mlab.com:35378/handraze-beta";
@@ -59,84 +58,88 @@ mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
+const PORT = process.env.PORT || 3000; // go to http://localhost:3000
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+server.listen(PORT, function () {
+  console.log(`Handraze Express Server listening on port ${PORT}`);
+});
 
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-  const PORT = process.env.PORT || 3000; // go to http://localhost:3000
-  const io = require("socket.io")(server);
+const io = require("socket.io")(server);
 
-io.on("connection", socket => {
-    socket.on('room', function(room) {
-      if(!rooms[room]){
-        rooms[room] = {}
-      }
-      socket.join(room);
-      if(rooms[room]["pdf"]){
-        let pdf = rooms[room]["pdf"]
-        io.in(room).emit("Get PDF", {pdf});
-      }
-      if(rooms[room]["page"]){
-        let page = rooms[room]["page"]
-        io.in(room).emit("Get Page", {page});
-      }
-    });
-    socket.on("Set PDF", msg => {
-      if(msg.pdf){
-        rooms[msg.lecture]["pdf"] = msg.pdf
-      }
-      var pdf = msg.pdf;
-      io.in(msg.lecture).emit("Get PDF", {pdf});
-    })
-    socket.on("Set Page", msg => {
-      if(msg.page){
-        rooms[msg.lecture]["page"] = msg.page
-      }
-      var page = msg.page
-      io.in(msg.lecture).emit("Get Page", {page});
-    })
-  
-  socket.on("Input Chat Message", msg => {
-    connect.then(db => {
+io.on("connection", (socket) => {
+  socket.on("room", function (room) {
+    if (!rooms[room]) {
+      rooms[room] = {};
+    }
+    socket.join(room);
+    if (rooms[room]["pdf"]) {
+      let pdf = rooms[room]["pdf"];
+      io.in(room).emit("Get PDF", { pdf });
+    }
+    if (rooms[room]["page"]) {
+      let page = rooms[room]["page"];
+      io.in(room).emit("Get Page", { page });
+    }
+  });
+  socket.on("Set PDF", (msg) => {
+    if (msg.pdf) {
+      rooms[msg.lecture]["pdf"] = msg.pdf;
+    }
+    var pdf = msg.pdf;
+    io.in(msg.lecture).emit("Get PDF", { pdf });
+  });
+  socket.on("Set Page", (msg) => {
+    if (msg.page) {
+      rooms[msg.lecture]["page"] = msg.page;
+    }
+    var page = msg.page;
+    io.in(msg.lecture).emit("Get Page", { page });
+  });
+
+  socket.on("Input Chat Message", (msg) => {
+    connect.then((db) => {
       try {
-        let chat = new Chat({message: msg.chatMessage, sender: msg.userId, room: msg.room, type: msg.type })
-        chat.save((err,doc)=> {
-          if(err) return res.json({success: false, err})
-          Chat.find({room: msg.room})
-          .populate("sender")
-          .exec((err, doc) => {
-            socket.join(msg.room);
-            //console.log(doc)
-            return io.to(msg.room).emit("Output Chat Message", doc.slice(-1).pop());
-          })
-        })
+        let chat = new Chat({
+          message: msg.chatMessage,
+          sender: msg.userId,
+          room: msg.room,
+          type: msg.type,
+        });
+        chat.save((err, doc) => {
+          if (err) return res.json({ success: false, err });
+          Chat.find({ room: msg.room })
+            .populate("sender")
+            .exec((err, doc) => {
+              socket.join(msg.room);
+              //console.log(doc)
+              return io
+                .to(msg.room)
+                .emit("Output Chat Message", doc.slice(-1).pop());
+            });
+        });
       } catch (error) {
         console.error(error);
       }
-    })
-  })
-
-
-})
-
+    });
+  });
+});
 
 //app.listen(PORT, function() {
- 
+
 //});
